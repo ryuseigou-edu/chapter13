79760eb6c5367fbdb90dc3e87489c96bf1e1038f
RyuseiNakaso <241101@std.hi-joho.ac.jp>
2025-07-04 11:51:18 +0900

    値の妥当性チェック

--- src/Hero.java
+++ src/Hero.java
@@ -1,28 +1,33 @@
 public class Hero {
     private int hp;
-    String name;
-    Sword sword;
+    private String name;
+    private Sword sword;
 
     public String getName() {
         return this.name;
     }
-    public void setName(String name) {
+    public void setName(String name) {// 妥当性チェック
+        if (name == null) {
+            throw new IllegalArgumentException("名前がnullである。処理を中断。");
+        }
+        if (name.length() <= 1) {
+            throw new IllegalArgumentException("名前が短すぎる。処理を中断。");
+        }
+        if (name.length() >= 8) {
+            throw new IllegalArgumentException("名前が長すぎる。処理を中断。");
+        }
         this.name = name;
     }
-    public void bye() {
+    void bye() {
         System.out.println("勇者は別れを告げた");
     }
     private void die() {
         System.out.println(this.name + "は死んでしまった！");
         System.out.println("GAME OVERです。");
     }
-    public void seppuku() {
-        System.out.println(this.name + "は切腹した！");
-        this.die();
-    }
-    public void sleep() {
+    void sleep() {
         this.hp = 100;
-        System.out.println(this.name + "は眠って回復した！");
+        System.out.println(this.name + "は、眠って回復した！");
     }
     public void attack(Matango m) {
         System.out.println(this.name + "の攻撃！");
